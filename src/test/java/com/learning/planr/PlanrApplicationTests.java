2aabecf27c0b904f28f7ffd496d3f2b04bab958b
ZakariaeAkharraz <akharraz.2003@gmail.com>
2025-05-13 15:59:34 +0100

    initiate the project

---
+++ src/test/java/com/learning/planr/PlanrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.planr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
